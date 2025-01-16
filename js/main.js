cfd92819ec43bb42e4c06750853485702fb5e90f
= <contactme.abdelilah@gmail.com>
2025-01-16 15:18:15 +0100

    Upload the Update

---
+++ js/main.js
@@ -0,0 +1,12 @@
+let menuBtn = document.querySelector(".menu-btn i");
+
+
+menuBtn.onclick = function () {
+    document.querySelector(".links ul").classList.toggle("active");
+}
+
+document.addEventListener("click", function (event) {
+    if (event.target !== menuBtn) {
+        document.querySelector(".links ul").classList.remove("active");
+    }
+})
